adc73c8f917cf4fc5003df9c371736e9c3f9e27d
juanpaz90 <juan.pazmino@thoughtworks.com>
2025-10-10 17:50:12 -0500

    Refactor spotify_auth and add access_sk

---
+++ src/access_sk.py
@@ -0,0 +1,15 @@
+import os
+from google.cloud import secretmanager
+
+
+class SecretKey:
+    def __init__(self):
+        self.my_project_id = os.environ["PROJECT_ID"]
+        self.client = secretmanager.SecretManagerServiceClient()
+
+    def get_secret(self, key_name) -> str:
+        secret_name = f"projects/{self.my_project_id}/secrets/{key_name}/versions/latest"
+        response = self.client.access_secret_version(name=secret_name)
+        # --> access_secret_version
+        my_secret_value = response.payload.data.decode("UTF-8")
+        return my_secret_value
--- src/spotify_auth.py
+++ src/spotify_auth.py
@@ -1,24 +1,29 @@
 import spotipy
-from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
-import os
+from access_sk import SecretKey
 
-load_dotenv()
-CLIENT_ID = os.getenv('CLIENT_ID')
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-REDIRECT_URI = 'https://127.0.0.1:8080'
 
 # Define required scopes for accessing user data
 SCOPE = 'user-read-private user-read-email user-library-read user-top-read user-read-recently-played playlist-read-private'
+REDIRECT_URI = 'https://127.0.0.1:8080'
 
 
-def get_spotify_client():
-    """Initialize Spotify client with OAuth"""
-    auth_manager = SpotifyOAuth(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        redirect_uri=REDIRECT_URI,
-        scope=SCOPE
-    )
+class SpotifyAuth:
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.secret_key = SecretKey()
 
-    return spotipy.Spotify(auth_manager=auth_manager)
+    def get_spotify_client(self):
+        """Initialize Spotify client with OAuth"""
+        auth_manager = SpotifyOAuth(
+            client_id = self.secret_key.get_secret("client_id"),
+            client_secret = self.secret_key.get_secret("client_secret"),
+            redirect_uri=REDIRECT_URI,
+            scope=SCOPE
+        )
+
+        try:
+            return spotipy.Spotify(auth_manager = auth_manager)
+        except Exception as e:
+            print(f"ERROR: {e}")
