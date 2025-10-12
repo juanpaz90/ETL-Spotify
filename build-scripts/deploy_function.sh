fb36be8358394d5be452a43794f5cb090bc5eadd
juanpaz90 <juan.pazmino@thoughtworks.com>
2025-10-12 01:08:27 -0500

    Update deployment

--- build-scripts/deploy_function.sh
+++ build-scripts/deploy_function.sh
@@ -9,6 +9,7 @@ deploy_function() {
     --trigger-topic=spotify-etl \
     --max-instances=5 \
     --runtime=python312 \
+    --ingress-settings=internal-only
     --set-env-vars=PROJECT_ID=$PROJECT_ID \
     --service-account=etl-deploy@testing-etl-jep.iam.gserviceaccount.com \
     --build-service-account=projects/$PROJECT_ID/serviceAccounts/etl-deploy@testing-etl-jep.iam.gserviceaccount.com
