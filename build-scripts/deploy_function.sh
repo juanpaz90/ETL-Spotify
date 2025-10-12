ae87f3301c22aca89ac21674d3387d5a91f7d6a4
juanpaz90 <juan.pazmino@thoughtworks.com>
2025-10-12 00:07:40 -0500

    Specify build-service-account in deploy_function script

--- build-scripts/deploy_function.sh
+++ build-scripts/deploy_function.sh
@@ -12,7 +12,8 @@ deploy_function() {
     --timeout=540s \
     --memory=512MB \
     --set-env-vars=PROJECT_ID=$PROJECT_ID \
-    --service-account=etl-deploy@testing-etl-jep.iam.gserviceaccount.com
+    --service-account=etl-deploy@testing-etl-jep.iam.gserviceaccount.com \
+    --build-service-account=etl-deploy@testing-etl-jep.iam.gserviceaccount.com
 }
 
 deploy_function
