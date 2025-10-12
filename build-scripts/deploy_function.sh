c01ab3064173d9a9214aa64d048d30219f3a67f8
juanpaz90 <juan.pazmino@thoughtworks.com>
2025-10-12 00:24:27 -0500

    Fix typo

--- build-scripts/deploy_function.sh
+++ build-scripts/deploy_function.sh
@@ -2,7 +2,7 @@
 
 deploy_function() {
   gcloud functions deploy spotify_etl \
-    --gen2
+    --gen2 \
     --trigger-topic=spotify-etl \
     --no-allow-unauthenticated \
     --region=us-east1 \
@@ -13,7 +13,7 @@ deploy_function() {
     --memory=512MB \
     --set-env-vars=PROJECT_ID=$PROJECT_ID \
     --service-account=etl-deploy@testing-etl-jep.iam.gserviceaccount.com \
-    --build-service-account=projects/$PROJECT_ID/serviceAccounts/etl-deploy@testing-etl-jep.iam.gserviceaccount.com
+    --build-service-account=etl-deploy@testing-etl-jep.iam.gserviceaccount.com
 }
 
 deploy_function
