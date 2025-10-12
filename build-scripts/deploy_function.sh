18cbd9a81e50105223a0869c000c58cbde3aa671
juanpaz90 <juan.pazmino@thoughtworks.com>
2025-10-12 01:02:14 -0500

    Update deployment

--- build-scripts/deploy_function.sh
+++ build-scripts/deploy_function.sh
@@ -2,15 +2,13 @@
 
 deploy_function() {
   gcloud functions deploy spotify_etl \
-    --gen2 \
-    --trigger-topic=spotify-etl \
-    --no-allow-unauthenticated \
     --region=us-east1 \
-    --source=./src \
-    --runtime=python312 \
-    --max-instances=5 \
-    --timeout=540s \
     --memory=512MB \
+    --timeout=540s \
+    --source=./src \
+    --trigger-topic=spotify-etl \
+    --max-instances=5 \
+    --runtime=python312 \
     --set-env-vars=PROJECT_ID=$PROJECT_ID \
     --service-account=etl-deploy@testing-etl-jep.iam.gserviceaccount.com \
     --build-service-account=projects/$PROJECT_ID/serviceAccounts/etl-deploy@testing-etl-jep.iam.gserviceaccount.com
