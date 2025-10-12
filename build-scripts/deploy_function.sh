c3e5bf046955003e421ad5877260a2f6c844ece0
juanpaz90 <juan.pazmino@thoughtworks.com>
2025-10-12 00:11:48 -0500

    Update function deployment

--- build-scripts/deploy_function.sh
+++ build-scripts/deploy_function.sh
@@ -3,7 +3,6 @@
 deploy_function() {
   gcloud functions deploy spotify_etl \
     --trigger-topic=spotify-etlg\
-    --no-gen2 \
     --no-allow-unauthenticated \
     --region=us-east1 \
     --source=./src \
@@ -13,7 +12,7 @@ deploy_function() {
     --memory=512MB \
     --set-env-vars=PROJECT_ID=$PROJECT_ID \
     --service-account=etl-deploy@testing-etl-jep.iam.gserviceaccount.com \
-    --build-service-account=etl-deploy@testing-etl-jep.iam.gserviceaccount.com
+    --build-service-account=projects/$PROJECT_ID/serviceAccounts/etl-deploy@testing-etl-jep.iam.gserviceaccount.com
 }
 
 deploy_function
