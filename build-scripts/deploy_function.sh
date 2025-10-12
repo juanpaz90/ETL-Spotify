a1c7dd672a36ee5d35380877caf4fa4f69711038
juanpaz90 <juan.pazmino@thoughtworks.com>
2025-10-11 23:14:09 -0500

    Fix typos on deploy_function script

--- build-scripts/deploy_function.sh
+++ build-scripts/deploy_function.sh
@@ -4,14 +4,14 @@ deploy_function() {
   gcloud functions deploy spotify_etl \
     --trigger-topic=spotify-etlg\
     --no-gen2 \
-    --no-allow-unauthenticated
+    --no-allow-unauthenticated \
     --region=us-east1 \
     --source=./src \
     --runtime=python312 \
     --max-instances=5 \
     --timeout=540s \
     --memory=512MB \
-    --set-env-vars=PROJECT_ID=$PROJECT_ID
+    --set-env-vars=PROJECT_ID=$PROJECT_ID \
     --service-account=etl-run-task@testing-etl-jep.iam.gserviceaccount.com
 }
 
