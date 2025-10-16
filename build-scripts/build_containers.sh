536062b81407967b61d6afd5a16aad2beb2f2e18
juanpaz90 <juan.pazmino@thoughtworks.com>
2025-10-16 01:32:19 -0500

    Fix folder path

--- build-scripts/build_containers.sh
+++ build-scripts/build_containers.sh
@@ -5,7 +5,7 @@ SPOTIFY_EXTRACTOR_TAG="us-east1-docker.pkg.dev/$PROJECT_ID/$REPOSITORY_NAME/$IMA
 
 # Build spotify-data-extractor image
 echo " Build spotify-data-extractor container"
-docker build -t "$SPOTIFY_EXTRACTOR_TAG" -f 'build/spotify_extractor.Dockerfile' .
+docker build -t "$SPOTIFY_EXTRACTOR_TAG" -f 'build-scripts/spotify_extractor.Dockerfile' .
 if [ $? -ne 0 ]; then
   echo "Error: Failed to build spotify-data-extractor image."
   exit 1
