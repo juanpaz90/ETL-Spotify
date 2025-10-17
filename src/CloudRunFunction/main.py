b3ee41d1b15426fec3af7c47466e998596460ed6
juanpaz90 <juan.pazmino@thoughtworks.com>
2025-10-17 17:10:31 -0500

    Fix reguex return

--- src/CloudRunFunction/main.py
+++ src/CloudRunFunction/main.py
@@ -14,15 +14,18 @@ def get_file_name(cloud_event):
 
 def file_name_validation(file_path, file_name, bucket_name):
     # table_name and file_name have the same name, so I can use them without any problem
-    name = re.findall(r"(\btrack_details)", file_name)[0]
-    if name == "track_details_":
-        print(f"File name to be cleaned: {file_name}")
-        df_data = read_file_from_gcs(bucket_name, file_name)
-        df_clean = fix_genre_name(df_data)
-        copy_to_bigquery(df_clean, file_name)
-    else:
-        print(f"File name: {file_name}")
-        move_to_bigquery(file_path, file_name)
+    name = re.findall(r"(\btrack_details)", file_name)
+    try:
+        if name[0] == "track_details":
+            print(f"File name to be cleaned: {file_name}")
+            df_data = read_file_from_gcs(bucket_name, file_name)
+            df_clean = fix_genre_name(df_data)
+            copy_to_bigquery(df_clean, file_name)
+        else:
+            print(f"File name: {file_name}")
+            move_to_bigquery(file_path, file_name)
+    except Exception as e:
+        print(f"ERROR: {e}")
 
 
 @functions_framework.cloud_event
